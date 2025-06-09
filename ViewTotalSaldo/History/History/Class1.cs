ae4547e2f822bfe7b0b650c0869e062492bd1e1f
naufal453 <105023440+naufal453@users.noreply.github.com>
2025-06-10 03:48:01 +0700

    update

--- ViewTotalSaldo/History/History/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HistoryLibrary
-{
-    public class History
-    {
-        public string Nama { get; set; }
-        public string Pin { get; set; }
-        public decimal Saldo { get; set; }
-    }
-}
