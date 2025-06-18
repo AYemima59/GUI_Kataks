0c586914568c4940b7f01f910dfc7628c45ee785
Jagaaan-spec <mcpe.redflash24@gmail.com>
2025-06-19 00:03:42 +0700

    add hisory function along with the test

---
+++ MoneyManagement/MoneyManagement/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using MoneyManagement;
+
+namespace MoneyManagement
+{
+    class Program
+    {
+        static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+         .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            Console.WriteLine("Enter month :");
+            string month = Console.ReadLine();
+
+            var section = config.GetSection($"History:{month}");
+
+            if (section.Exists())
+            {
+                var income = section["Income"];
+                var spending = section["Spending"];
+
+                Console.WriteLine($"Income: {income}");
+                Console.WriteLine($"Spending: {spending}");
+            }
+            else
+            {
+                Console.WriteLine("Month data not found.");
+            }
+        }
+    }
+}
+
